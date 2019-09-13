d11d862d3a2a9e9279c9f0a3b66ae7095e1d2196
Janelle Cueto <janellecueto@gmail.com>
2019-09-12 21:08:25 -0500

    updated

--- app/users/views.py
+++ app/users/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    response = "<h1>Hello!</h1>"
-    return HttpResponse(response)
+    return render(request, 'users/home.html', {})
