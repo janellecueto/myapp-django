e5b7e270e4b4ae086fc5ab2ae5092b2a465558cd
Janelle Cueto <janellecueto@gmail.com>
2019-09-14 11:29:06 -0500

    implementing sqlite3

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    url(r'^home/$', views.home, name='home')
+    url(r'^index/$', views.index, name='index'),
+    url(r'^detail/(?P<user_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name='add'),
 ]
